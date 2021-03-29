b0ec890e557e199964fa1a6a316ce91447cf90ec
Robert Hollingworth <robert@roberts-air.home>
2021-03-29 19:50:44 +0100

    Added negative number test

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,10 +1,8 @@
-// https://docs.cypress.io/api/introduction/api.html
-
 describe('calculator', () => {
   beforeEach(() => {
     cy.visit('/')
   });
-
+// display negative
   it('should display the runnning total', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
@@ -26,7 +24,8 @@ describe('calculator', () => {
     cy.get('#number1').click();
     cy.get('#operator_equals').click();
     cy.get('.display').should('contain', '6')
-  }) 
+  })
+
   it('Should handle large numbers', () => {
     cy.get('#number2').click();
     cy.get('#number2').click();
@@ -49,19 +48,29 @@ describe('calculator', () => {
     cy.get('#number1').click();
     cy.get('#operator_equals').click();
     cy.get('.display').should('contain', '1133333333')
-  }) 
+  })
+
   it('Should handle decimal numbers', () => {
     cy.get('#number3').click();
     cy.get('#operator_divide').click();
     cy.get('#number2').click();
     cy.get('#operator_equals').click();
     cy.get('.display').should('contain', '1.5')
-  }) 
+  })
+
   it('Should handle dividing by 0', () => {
     cy.get('#number3').click();
     cy.get('#operator_divide').click();
     cy.get('#number0').click();
     cy.get('#operator_equals').click();
     cy.get('.display').should('contain', '0')
-  }) 
+  })
+
+  it('should be able to display numbers as negative', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number9').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '-4');
+  })
 })
